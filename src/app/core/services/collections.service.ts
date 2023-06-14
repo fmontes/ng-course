d641a6b7a49c475d5cdde4cc22d6d7208fe2f4ef
Freddy Montes <751424+fmontes@users.noreply.github.com>
2023-06-13 22:08:54 -0600

    Clean up

--- src/app/core/services/collections.service.ts
+++ src/app/core/services/collections.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { environment } from 'src/environments/environment.development';
-import { LinksRecord, LinksResponse } from '../types/pocketbase-types';
+import { LinksRecord } from '../types/pocketbase-types';
 import { ListResult } from 'pocketbase';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';

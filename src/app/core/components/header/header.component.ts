04711b14a8f9af078786a63fee63e80251570042
Freddy Montes <751424+fmontes@users.noreply.github.com>
2023-06-18 13:08:11 -0600

    Add home router link

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavUserInfoComponent } from '../nav-user-info/nav-user-info.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, NavUserInfoComponent],
+  imports: [CommonModule, NavUserInfoComponent, RouterLink],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })

a2604c0a2097aa2bda14e115ee378dedb1a24ca6
Freddy Montes <751424+fmontes@users.noreply.github.com>
2023-06-24 21:38:00 -0600

    Add titles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { Routes } from '@angular/router';
+import { ActivatedRouteSnapshot, Routes } from '@angular/router';
 
 import { LoginComponent } from './pages/login/login.component';
 import { EditComponent } from './pages/edit/edit.component';
@@ -10,6 +10,20 @@ import { ProfileNotFoundComponent } from './pages/profile-not-found/profile-not-
 import { RegisterComponent } from './pages/register/register.component';
 import { registerGuard } from './core/guards/register.guard';
 import { LayoutComponent } from './core/components/layout/layout.component';
+import { UserService } from './core/services/user.service';
+import { inject } from '@angular/core';
+import { map } from 'rxjs/operators';
+
+function getNameTitle(route: ActivatedRouteSnapshot) {
+  const userService = inject(UserService);
+  const username: string = route.params['username'];
+
+  return userService
+    .getByUsername(username)
+    .pipe(map((user) => `${NAME} - ${user.name}`));
+}
+
+const NAME = 'EchoLink';
 
 export const routes: Routes = [
   {
@@ -19,24 +33,29 @@ export const routes: Routes = [
       {
         path: '',
         component: HomeComponent,
+        title: `${NAME} - Create and share your own links`,
       },
       {
         path: 'login',
         component: LoginComponent,
+        title: `${NAME} - Login`,
       },
       {
         path: 'register',
         component: RegisterComponent,
         canActivate: [registerGuard],
+        title: `${NAME} - Register`,
       },
       {
         path: 'profile-not-found',
         component: ProfileNotFoundComponent,
+        title: `${NAME} - Profile not found`,
       },
       {
         path: ':username/edit',
         component: EditComponent,
         canActivate: [profileGuard],
+        title: getNameTitle,
         resolve: {
           data: profileResolver,
         },
@@ -46,6 +65,7 @@ export const routes: Routes = [
   {
     path: ':username',
     component: ProfileComponent,
+    title: getNameTitle,
     resolve: {
       data: profileResolver,
     },
